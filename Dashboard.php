b740ba996825b47f40f18e065de3babeb59b9e38
Zeetito <agyareernest44@gmail.com>
2022-11-02 12:41:19 +0000

    Added strings to Dash and hompage

--- Dashboard.php
+++ Dashboard.php
@@ -1,4 +1,5 @@
 <?php include("actions/auth.php")?>
 <?php include("header2.php")?>
 
-<p> Hello This is the Dashboard man...</p>
+<p> Hello This is the Dashboard man...
+Testing some stuff out thoguth</p>
--- homepage.php
+++ homepage.php
@@ -2,7 +2,7 @@
 <?php echo"Hello ".$_SESSION['username'].", Good Day!!"; ?>
 
 
-<p>Welcome to the Homepage....</p>
+<p>Welcome to the Homepage...Just testing some stuff out tho.</p>
 
 
 <?php include('footer2.php'); ?>
